371d5371cdd40945c8c4a0a9cb06d890346d309c
tusharpatil3688 <tusharpatil3688@gmail.com>
2020-10-13 23:16:53 +0530

    fix:* fetch serial no in issue * set read only if type is black&white * * remove bug from task table

--- mfi_customization/mfi/doctype/issue.py
+++ mfi_customization/mfi/doctype/issue.py
@@ -8,31 +8,11 @@ from frappe.model.mapper import get_mapped_doc
 
 @frappe.whitelist()
 def make_task(source_name, target_doc=None):
-	def set_missing_values(source, target):
-		target.asset_=source.asset
-		# target.set('older__reading',[])
-		for d in source.get('current_reading'):
-			target.append("current_reading", {
-				"date":d.get('date'),
-				"type":d.get('type'),
-				"asset":d.get('asset'),
-				"reading":d.get('reading')
-			})
-		if source.project:
-			pro_doc=frappe.get_doc('Project',source.project)
-			# for d in pro_doc.get('machine_readings'):
-			# 	if d.get('asset')==source.asset:
-			# 		target.append("older__reading", {
-			# 			"date":d.get('date'),
-			# 			"type":d.get('type'),
-			# 			"asset":d.get('asset'),
-			# 			"reading":d.get('reading')
-			# 		})
 	return get_mapped_doc("Issue", source_name, {
 		"Issue": {
 			"doctype": "Task"
 		}
-	}, target_doc,set_missing_values)
+	}, target_doc)
 
 @frappe.whitelist()
 def get_asset_list(doctype, txt, searchfield, start, page_len, filters):
