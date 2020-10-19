feeb1ae7d0420f6b2d715b32df790227ac8926ae
tusharpatil3688 <tusharpatil3688@gmail.com>
2020-10-19 14:57:53 +0530

    corrected time resolution column formatting

--- mfi_customization/mfi/report/issue_timelines/issue_timelines.py
+++ mfi_customization/mfi/report/issue_timelines/issue_timelines.py
@@ -3,6 +3,7 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe.utils import getdate
 
 def execute(filters=None):
 	data = prepare_data(filters)
@@ -83,8 +84,25 @@ def prepare_data(filters):
 		import datetime
 		t = i.resolution_date
 		if i.resolution_date:
-			time_resolution=datetime.timedelta(hours=int(t.strftime('%H')),minutes=int(t.strftime('%M')), seconds=int(t.strftime('%S')))-i.opening_time
-			i.update({'time_resolution':str(time_resolution)})
+			days=0
+			if getdate(t)==getdate(i.opening_date):
+				time_resolution=datetime.timedelta(hours=int(t.strftime('%H')),minutes=int(t.strftime('%M')), seconds=int(t.strftime('%S')))-i.opening_time
+				totsec = time_resolution.total_seconds()
+				h = totsec//3600
+				m = (totsec%3600) // 60
+				sec =(totsec%3600)%60 
+				i.update({'time_resolution':"D:%d  H: %d M: %d S: %d" %(days,h,m,sec)})
+			else:
+				days=(getdate(t)-getdate(i.opening_date)).days
+				close=datetime.timedelta(hours=int(t.strftime('%H')),minutes=int(t.strftime('%M')), seconds=int(t.strftime('%S')))
+				time_resolution=i.opening_time-close
+				if close>i.opening_time:
+					time_resolution=close-i.opening_time
+				totsec = time_resolution.total_seconds()
+				h = totsec//3600
+				m = (totsec%3600) // 60
+				sec =(totsec%3600)%60 
+				i.update({'time_resolution':"D:%d  H: %d M: %d S: %d" %(days,h,m,sec)})
 		data.append(i)
 	return data
 
