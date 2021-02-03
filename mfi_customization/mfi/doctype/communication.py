1c013ad9fe1dfbb0c216479be413b62b1d4cc934
tusharpatil3688 <tusharpatil3688@gmail.com>
2021-02-03 23:49:59 +0530

    fix:attachment issue

--- mfi_customization/mfi/doctype/communication.py
+++ mfi_customization/mfi/doctype/communication.py
@@ -1,33 +1,40 @@
 import frappe
 
-def after_insert(doc,method):
-    domain_rule=email_rules_true_for_domain(doc.sender)
-    email_rule=email_rules_true_for_emails_table(doc.sender)
-    print('************************************************')
-    print(domain_rule, email_rule)
-    if (domain_rule.get('is_true') or email_rule.get('is_true')) and doc.sent_or_received=='Received':
-        issue=frappe.new_doc("Issue")
-        issue.subject=doc.subject
-        issue.description=doc.content
-        issue.raised_by=doc.sender
-        issue.customer=domain_rule.get('customer') if domain_rule.get('is_true') else  email_rule.get('customer')
-        issue.flags.ignore_mandatory=True
-        issue.company=domain_rule.get('company') if domain_rule.get('is_true') else  email_rule.get('company')
-        issue.save()
-
 def after_insert_file(doc,method):
     if doc.attached_to_doctype=="Communication":
-        file_doc = frappe.new_doc("File")
-        file_doc.file_name = doc.file_name
-        file_doc.file_size = doc.file_size
-        file_doc.folder = doc.folder
-        file_doc.is_private = doc.is_private
-        file_doc.file_url = doc.file_url
-        file_doc.attached_to_doctype = "Issue"
-        issue_list=frappe.get_all('Issue',order_by='name desc', limit=1)
-        if issue_list:
-            file_doc.attached_to_name=issue_list[0].get('name')
-            file_doc.save()
+        if len(frappe.get_all('Issue',{'communication':doc.attached_to_name}))==0:
+            cmm_doc=frappe.get_doc("Communication",doc.attached_to_name)
+            domain_rule=email_rules_true_for_domain(cmm_doc.sender)
+            email_rule=email_rules_true_for_emails_table(cmm_doc.sender)
+            if (domain_rule.get('is_true') or email_rule.get('is_true')) and cmm_doc.sent_or_received=='Received':
+                issue=frappe.new_doc("Issue")
+                issue.subject=cmm_doc.subject
+                issue.description=cmm_doc.content
+                issue.raised_by=cmm_doc.sender
+                issue.customer=domain_rule.get('customer') if domain_rule.get('is_true') else  email_rule.get('customer')
+                issue.flags.ignore_mandatory=True
+                issue.company=domain_rule.get('company') if domain_rule.get('is_true') else  email_rule.get('company')
+                issue.save()
+                file_doc = frappe.new_doc("File")
+                file_doc.file_name = doc.file_name
+                file_doc.file_size = doc.file_size
+                file_doc.folder = doc.folder
+                file_doc.is_private = doc.is_private
+                file_doc.file_url = doc.file_url
+                file_doc.attached_to_doctype = "Issue"
+                file_doc.attached_to_name=issue.get('name')
+                file_doc.save()
+        else:
+            for d in frappe.get_all('Issue',{'communication':doc.attached_to_name},['name']):
+                file_doc = frappe.new_doc("File")
+                file_doc.file_name = doc.file_name
+                file_doc.file_size = doc.file_size
+                file_doc.folder = doc.folder
+                file_doc.is_private = doc.is_private
+                file_doc.file_url = doc.file_url
+                file_doc.attached_to_doctype = "Issue"
+                file_doc.attached_to_name=d.get('name')
+                file_doc.save()
 
 def email_rules_true_for_domain(sender):
     resp={'is_ture':False,'customer':'','company':''}
