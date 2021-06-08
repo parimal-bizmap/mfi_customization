753e96972a2d2433248437f745193a959ccfab62
tusharpatil3688 <tusharpatil3688@gmail.com>
2021-06-08 22:45:35 +0530

    fix:material_request_approver from employee

--- mfi_customization/mfi/doctype/material_request.py
+++ mfi_customization/mfi/doctype/material_request.py
@@ -1,9 +1,9 @@
 import frappe
 
 def validate(doc,method):
-	for emp in frappe.get_all("Employee",{"user_id":frappe.session.user},['reports_to']):
-		if emp.reports_to:
-			for emp2 in frappe.get_all("Employee",{"name":emp.reports_to},['user_id']):
+	for emp in frappe.get_all("Employee",{"user_id":frappe.session.user},['material_request_approver']):
+		if emp.material_request_approver:
+			for emp2 in frappe.get_all("Employee",{"name":emp.material_request_approver},['user_id']):
 				if emp2.user_id:
 					doc.approver=emp2.user_id
 					doc.approver_name=frappe.db.get_value("User",emp2.user_id,"full_name")
