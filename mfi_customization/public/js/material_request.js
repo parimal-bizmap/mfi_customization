395b5eadede69d9ab2851d208138d4f97b4dba44
parimal-bizmap <parimal.patil@bizmap.in>
2021-06-11 00:47:48 +0530

    mr fixed

--- mfi_customization/public/js/material_request.js
+++ mfi_customization/public/js/material_request.js
@@ -1,30 +1,32 @@
-frappe.ui.form.on('Issue', {
-	onload: function ( frm ) {
-        frappe.call({
-            method: "mfi_customization.mfi.doctype.material_request.get_approver",
-            args: {
-                "user": frappe.session.user
-            },
-            callback: function(r) {
-            
-                    frm.set_value('approver',r.message);				
-                }
+// frappe.ui.form.on('Issue', {
+// 	onload: function ( frm ) {
+//         frappe.call({
+//             method: "mfi_customization.mfi.doctype.material_request.get_approver",
+//             args: {
+//                 "user": frappe.session.user
+//             },
+//             callback: function(r) {
+
+//                 console.log(r.message);
+//                     frm.set_value('approver',r.message);				
+//                 }
     
             
-    });
-    frappe.call({
-        method: "mfi_customization.mfi.doctype.material_request.get_approver_name",
-        args: {
-            "user": frappe.session.user
-        },
-        callback: function(r) {
-        
-                frm.set_value('approver_name',r.message);				
-            }
+//     });
+//     frappe.call({
+//         method: "mfi_customization.mfi.doctype.material_request.get_approver_name",
+//         args: {
+//             "user": frappe.session.user
+//         },
+//         callback: function(r) {
+//                 console.log(r.message);
+//                 frm.set_value('approver_name',r.message);		
+                		
+//             }
 
         
-});
+// });
 
     
-    }
-});
+//     }
+// });
