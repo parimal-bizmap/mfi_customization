58b474275ea732294cd5361162f365892151f8db
tusharpatil3688 <tusharpatil3688@gmail.com>
2021-06-23 00:20:52 +0530

    fix:added itemcode query in MR

--- mfi_customization/mfi/doctype/material_request.py
+++ mfi_customization/mfi/doctype/material_request.py
@@ -1,4 +1,8 @@
 import frappe
+from frappe.desk.reportview import get_match_cond, get_filters_cond
+from frappe.utils import nowdate, getdate
+
+
 
 def validate(doc,method):
 	for emp in frappe.get_all("Employee",{"user_id":frappe.session.user},['material_request_approver']):
@@ -75,6 +79,75 @@ def get_approver(user):
 def get_approver_name(user):
 
 	return frappe.db.get_value("User",{"email":user},"full_name")
+
+
+@frappe.whitelist()
+@frappe.validate_and_sanitize_search_inputs
+def item_query(doctype, txt, searchfield, start, page_len, filters, as_dict=False):
+	conditions = []
+
+	#Get searchfields from meta and use in Item Link field query
+	meta = frappe.get_meta("Item", cached=True)
+	searchfields = meta.get_search_fields()
+
+	if "description" in searchfields:
+		searchfields.remove("description")
+
+	columns = ''
+	extra_searchfields = [field for field in searchfields
+		if not field in ["name", "item_group", "description"]]
+
+	if extra_searchfields:
+		columns = ", " + ", ".join(extra_searchfields)
+
+	searchfields = searchfields + [field for field in[searchfield or "name", "item_code", "item_group", "item_name"]
+		if not field in searchfields]
+	searchfields = " or ".join([field + " like %(txt)s" for field in searchfields])
+	item_group_list=''
+	if filters.get("item_group"):
+		item_group_list=",".join(['"'+d.name+'"' for d in frappe.get_all("Item Group",{"parent_item_group":filters.get("item_group")})])
+	custom_condition=''
+	if item_group_list:
+		custom_condition=(" and `tabItem`.item_group IN ("+item_group_list+")")
+
+	description_cond = ''
+	if frappe.db.count('Item', cache=True) < 50000:
+		# scan description only if items are less than 50000
+		description_cond = 'or tabItem.description LIKE %(txt)s'
+
+	return frappe.db.sql("""select tabItem.name,
+		if(length(tabItem.item_name) > 40,
+			concat(substr(tabItem.item_name, 1, 40), "..."), item_name) as item_name,
+		tabItem.item_group,
+		if(length(tabItem.description) > 40, \
+			concat(substr(tabItem.description, 1, 40), "..."), description) as description
+		{columns}
+		from tabItem
+		where tabItem.docstatus < 2
+			and tabItem.has_variants=0
+			and tabItem.disabled=0
+			and (tabItem.end_of_life > %(today)s or ifnull(tabItem.end_of_life, '0000-00-00')='0000-00-00')
+			and ({scond} or tabItem.item_code IN (select parent from `tabItem Barcode` where barcode LIKE %(txt)s)
+				{description_cond})
+			 {mcond} {custom_condition}
+		order by
+			if(locate(%(_txt)s, name), locate(%(_txt)s, name), 99999),
+			if(locate(%(_txt)s, item_name), locate(%(_txt)s, item_name), 99999),
+			idx desc,
+			name, item_name
+		limit %(start)s, %(page_len)s """.format(
+			columns=columns,
+			scond=searchfields,
+			mcond=get_match_cond(doctype).replace('%', '%%'),
+			custom_condition=custom_condition,
+			description_cond = description_cond),
+			{
+				"today": nowdate(),
+				"txt": "%%%s%%" % txt,
+				"_txt": txt.replace("%", ""),
+				"start": start,
+				"page_len": page_len
+			}, as_dict=as_dict)
 
 
 					
