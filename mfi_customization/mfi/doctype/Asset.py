f7870d6fe9da68272d822d3ff4d1c64f1be3cf92
tusharpatil3688 <tusharpatil3688@gmail.com>
2021-07-20 12:05:05 +0530

    fix:sales invoice customization

--- mfi_customization/mfi/doctype/Asset.py
+++ mfi_customization/mfi/doctype/Asset.py
@@ -1,4 +1,6 @@
 import frappe
+from frappe.utils import flt
+
 def after_insert(doc,method):
     if frappe.db.exists('Asset Serial No', doc.serial_no):
         frappe.throw("serial number already exist")
@@ -18,3 +20,51 @@ def on_cancel(doc,method):
 def on_update(doc, method):
     #updating location in Serial No doctype with Asset location change.
  	frappe.db.set_value('Asset Serial No',doc.serial_no,'location',doc.location)
+
+
+@frappe.whitelist()
+def get_assets(project):
+    data=[]
+    sales_order_doc=""
+    mono_per_click_rate=0
+    colour_per_click_rate=0
+    if frappe.db.get_value("Project",{"name":project},"sales_order"):
+        sales_order_doc=frappe.get_doc("Sales Order",frappe.db.get_value("Project",{"name":project},"sales_order"))
+        mono_per_click_rate=sales_order_doc.mono_per_click_rate
+        colour_per_click_rate=sales_order_doc.colour_per_click_rate
+    for row in frappe.get_all("Asset",{"project":project},["item_code","location","name","serial_no","item_name"]):
+        mono_current_reading=0
+        colour_current_reading=0
+        mono_last_reading=0
+        colour_last_reading=0
+        for i,d in enumerate(get_reading(row.name)):
+            if i==0:
+                mono_current_reading=d.black_and_white_reading
+                colour_current_reading=d.colour_reading
+
+            else:
+                mono_last_reading=d.black_and_white_reading
+                colour_last_reading=d.colour_reading
+        data.append(
+            {
+                "item_code":row.item_code,
+                "location":row.location,
+                "name":row.name,
+                "serial_no":row.serial_no,
+                "item_name":row.item_name,
+                "mono_per_click_rate":mono_per_click_rate,
+                "colour_per_click_rate":colour_per_click_rate,
+                "monocurrent_reading":mono_current_reading,
+                "mono_last_reading":mono_last_reading,
+                "colour_current_reading":colour_current_reading,
+                "colour_last_reading":colour_last_reading,
+                "total_mono_charges":(flt(mono_current_reading)- flt(mono_last_reading))*mono_per_click_rate,
+                "total_colourcharges":(flt(colour_current_reading)-flt(colour_last_reading))*colour_per_click_rate,
+                "rate":((flt(mono_current_reading) - flt(mono_last_reading))*mono_per_click_rate)+((flt(colour_current_reading)-flt(colour_last_reading))*colour_per_click_rate)
+            }
+        )
+    
+    return data
+
+def get_reading(asset):
+    return frappe.get_all("Machine Reading",filters={"asset":asset,"reading_type":"Billing"},fields=["colour_reading","black_and_white_reading"],limit=2)
---
+++ mfi_customization/public/js/sales_invoice.js
@@ -0,0 +1,42 @@
+frappe.ui.form.on('Sales Invoice', {
+	get_assets:function(frm){
+        if (frm.doc.project){
+            // frm.add_custom_button('Get Assets', function() {
+                frappe.call({
+                    method: "mfi_customization.mfi.doctype.Asset.get_assets",
+                    args: {
+                        "project": frm.doc.project
+                    },
+                    callback: function(r) {
+                       if (r.message){
+                            frm.clear_table("items");
+                            $.each(r.message || [], function(i, d) {
+                                var row=frm.add_child("items")
+                                row.item_code=d.item_code
+                                row.item_name=d.item_name
+                                row.location=d.location
+                                row.asset=d.name
+                                row.serial_no=d.serial_no
+                                row.mono_per_click_rate=d.mono_per_click_rate
+                                row.colour_per_click_rate=d.colour_per_click_rate
+                                row.mono_per_click_rate=d.mono_per_click_rate
+                                row.colour_per_click_rate=d.colour_per_click_rate
+                                row.total_mono_charges=d.total_mono_charges
+                                row.total_colourcharges=d.total_colourcharges
+                                row.monocurrent_reading=d.monocurrent_reading
+                                row.mono_last_reading=d.mono_last_reading
+                                row.colour_current_reading=d.colour_current_reading
+                                row.colour_last_reading=d.colour_last_reading
+                                row.rate=d.rate
+                                    
+                            });
+                            frm.refresh_field("items")
+                       }
+                    }
+            
+                });
+            // });
+        }
+
+	}
+})
