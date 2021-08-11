541332aa881179a68bd8c7cf0e4555fb9baa44e2
tusharpatil3688 <tusharpatil3688@gmail.com>
2021-08-11 19:53:43 +0530

    fix:task and issue datetime field set changes

--- mfi_customization/mfi/patch/set_first_responded_on_issue.py
+++ mfi_customization/mfi/patch/set_first_responded_on_issue.py
@@ -4,12 +4,7 @@ from datetime import datetime
 # bench execute mfi_customization.mfi.patch.set_first_responded_on_issue.execute
 def execute():
 	for d in frappe.get_all("Issue"):
-		issue=frappe.get_doc("Issue",d.name)
-		task_data = frappe.db.get_value("Task", {"issue": issue.name}, ['attended_date_time', 'status'], as_dict=1)
-		if task_data:	
-			if task_data.attended_date_time:
-				issue.first_responded_on = task_data.attended_date_time
-				issue.save()
-			elif issue.status == 'Working' or task_data.get('status') == 'Working' :
-				issue.first_responded_on = datetime.now()
-				issue.save()
+		for tk in frappe.get_all("Task",{"issue": d.name}, ['attended_date_time', 'status']):
+			if tk.attended_date_time:
+				frappe.db.set_value("Issue", {"name": d.name},"first_responded_on",tk.attended_date_time)
+			
