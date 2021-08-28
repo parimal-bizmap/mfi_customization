8773b9aa108b3a24f9b865cee25e1b7b6b00e651
tusharpatil3688 <tusharpatil3688>
2021-08-28 20:30:00 +0530

    fix:machine reading tool bugs

--- mfi_customization/mfi/doctype/machine_reading_tool/machine_reading_tool.js
+++ mfi_customization/mfi/doctype/machine_reading_tool/machine_reading_tool.js
@@ -1,14 +1,15 @@
 // Copyright (c) 2021, bizmap technologies and contributors
 // For license information, please see license.txt
-
 frappe.ui.form.on('Machine Reading Tool', {
+	refresh:function(frm){
+		frm.set_value("project",'')
+		frm.disable_save();
+		frm.page.clear_indicator();
+		frm.set_value("reading_date",frappe.datetime.get_today())
+	},
 	project: function(frm) {
-		console.log("@@@@@@@@@@frm.doc.project", frm.doc.project)
 		frm.doc.show_submit = false;
 		if(frm.doc.project) {
-			console.log("@@@@@@@@@@frm.doc.project", frm.doc.project)
-			// if (!frm.doc.student_group)
-			// 	return
 			frappe.call({
 				method: "mfi_customization.mfi.doctype.machine_reading_tool.machine_reading_tool.get_machine_reading",
 				args: {
@@ -16,8 +17,6 @@ frappe.ui.form.on('Machine Reading Tool', {
 				},
 				callback: function(r) {
 					if (r.message) {
-						console.log("!!!!!!!!!!!!!!!!! r.message", r.message)
-						// frm.doc.students = r.message;
 						frm.events.render_table(frm, r.message);
 						for (let value of r.message) {
 							if (!value.docstatus) {
@@ -25,134 +24,137 @@ frappe.ui.form.on('Machine Reading Tool', {
 								break;
 							}
 						}
-						// frm.events.submit_result(frm);
+						frm.events.submit_result(frm);
 					}
 				}
 			});
 		}
 	},
-	render_table: function(frm, result) {
-		console.log("@@@@@@@@@@result ", result)
+
+	render_table: function(frm,asset_list) {
 		$(frm.fields_dict.reading_items.wrapper).empty();
-		let project = frm.doc.project;
-		frm.events.get_reading(frm, result);
-
-
-
-		// frappe.call({
-		// 	method: "erpnext.education.api.get_assessment_details",
-		// 	args: {
-		// 		project: project
-		// 	},
-		// 	callback: function(r) {
-		// 		frm.events.get_marks(frm, r.message);
-		// 	}
-		// });
+		frm.events.get_marks(frm,asset_list);
 	},
-	get_reading: function(frm, result_list) {
-		console.log("////////////result_list", result_list)
-		let max_total = 0;
-		result_list.forEach(function(c) {
-			max_total += c.total
-		});
+
+	get_marks: function(frm,asset_list) {
 		var result_table = $(frappe.render_template('machine_reading_tool', {
 			frm: frm,
-			project: frm.doc.project,
-			machine_reading_list: result_list,
-			max_total: max_total
+			assets: asset_list,
 		}));
 		result_table.appendTo(frm.fields_dict.reading_items.wrapper);
 
 		result_table.on('change', 'input', function(e) {
+			
 			let $input = $(e.target);
-			// let reading = $input.data();
 			let asset = $input.data().asset;
-			// let max_score = $input.data().maxScore;
+			let max_score = $input.data().maxScore;
 			let value = $input.val();
 			if(value < 0) {
 				$input.val(0);
+			} else if(value > max_score) {
+				$input.val(max_score);
 			}
-			 // else if(value > max_score) {
-			// 	$input.val(max_score);
-			// }
-			let total_reading = 0;
-			let machine_reading = {};
-			machine_reading["asset_reading"] = {};
-			result_table.find(`input[data-asset=${asset}].asset-result-data`)
-				.each(function(el, input) {
-					debugger;
-					console.log("#333el ", el)
-					let $input = $(input);
-					console.log("$input.data()",$input.data())
-					console.log($input[0].dataset['asset']);
-					let reading = $input.data().machine_reading_list;
-					// let machine_type = $input.data().machine_type;
-					// let value = parseFloat($input.val());
-					// let colour_reading = $input.data().colour_reading;
-					console.log("!!!!!!!!!!! reading", reading)
-
-					// let black_and_white_reading = $input.data().black_and_white_reading;
-					// console.log("!!!!!!!!!!! black_and_white_reading", black_and_white_reading)
-					
-					let value = parseFloat($input.val());
-                    console.log("!!!!!!!!!!! value", value)
-					if (!Number.isNaN(value)) {
-						machine_reading["asset_reading"][reading] = value;
-						// machine_reading["asset_reading"][colour_reading] = value.colour_reading;
-					}
-					total_reading += value.total;
+			let total_score = 0;
+			let asset_readings = {};
+	
+			asset_readings["reading_details"] = {}
+			asset_readings["reading_details"]["asset"] = asset;
+			result_table.find(`[data-asset=${asset}].result-bw-reading`).each(function(el, input){
+				asset_readings["reading_details"]["bw_reading"] = $(input).val();
 			});
-			console.log("asset_reading", machine_reading["asset_reading"])
-			if(!Number.isNaN(total_reading)) {
-				result_table.find(`span[data-asset=${asset}].total-reading`).html(total_reading);
-			    console.log("!!!!!!!!!!!! total_reading",total_reading)
-			}console.log("result_list.length", result_list.length)
-			console.log("asset_reading_length", Object.keys(machine_reading["asset_reading"]).length)
-			if (Object.keys(machine_reading["asset_reading"]).length > 1) {
-				// machine_reading["readings"] = readings;
-				machine_reading["total_reading"] = total_reading;
-				result_table.find(`[data-asset=${asset}].result-black_and_white_reading`)
-					.each(function(el, input){
-					machine_reading["asset_reading"][black_and_white_reading] = $(input).val();
-				});
-				result_table.find(`[data-asset=${asset}].result-colour_reading`)
-					.each(function(el, input){
-					machine_reading["asset_reading"][colour_reading] = $(input).val();
-				});
-				result_table.find(`[data-asset=${asset}].result-comment`)
-					.each(function(el, input){
-					machine_reading["comment"] = $(input).val();
-				});
-				console.log('!!!!!!!!!!!!!!machine_reading', machine_reading)
+			result_table.find(`[data-asset=${asset}].result-colour-reading`).each(function(el, input){
+				asset_readings["reading_details"]["colour"] = $(input).val();
+			});
+			result_table.find(`[data-asset=${asset}].result-reading_type`).each(function(el, input){
+				asset_readings["reading_details"]["reading_type"] = $(input).val();
+			});
+			result_table.find(`[data-asset=${asset}].result-total_reading`).each(function(el, input){
+				asset_readings["reading_details"]["total_reading"] = $(input).val();
+			});
+			frappe.call({
+				method: "mfi_customization.mfi.doctype.machine_reading_tool.machine_reading_tool.create_machine_reading",
+				args: {
+					"readings": asset_readings
+				},
+				callback: function(r) {
+					console.log(r.message)
+					// let assessment_result = r.message;
+					// if (!frm.doc.show_submit) {
+					// 	frm.doc.show_submit = true;
+					// 	frm.events.submit_result;
+					// }
+					// for (var criteria of Object.keys(assessment_result.details)) {
+					// 	result_table.find(`[data-criteria=${criteria}][data-asset=${assessment_result
+					// 		.asset}].asset-result-grade`).each(function(e1, input) {
+					// 			$(input).html(assessment_result.details[criteria]);
+					// 	});
+					// }
+					// result_table.find(`span[data-asset=${assessment_result.asset}].total-score-grade`).html(assessment_result.grade);
+					// let link_span = result_table.find(`span[data-asset=${assessment_result.asset}].total-result-link`);
+					// $(link_span).css("display", "block");
+					// $(link_span).find("a").attr("href", "/app/assessment-result/"+assessment_result.name);
+				}
+			});
+			// if (Object.keys(asset_readings["reading_details"]).length === criteria_list.length) {
+			// 	asset_readings["asset"] = asset;
+			// 	asset_readings["total_score"] = total_score;
+
+
+			// 	result_table.find(`[data-asset=${asset}].result-comment`)
+			// 		.each(function(el, input){
+			// 		asset_readings["comment"] = $(input).val();
+			// 	});
+			// 	console.log(asset_readings)
+			// 	frappe.call({
+			// 		method: "erpnext.education.api.mark_assessment_result",
+			// 		args: {
+			// 			"assessment_plan": frm.doc.assessment_plan,
+			// 			"scores": asset_readings
+			// 		},
+			// 		callback: function(r) {
+			// 			let assessment_result = r.message;
+			// 			if (!frm.doc.show_submit) {
+			// 				frm.doc.show_submit = true;
+			// 				frm.events.submit_result;
+			// 			}
+			// 			for (var criteria of Object.keys(assessment_result.details)) {
+			// 				result_table.find(`[data-criteria=${criteria}][data-asset=${assessment_result
+			// 					.asset}].asset-result-grade`).each(function(e1, input) {
+			// 						$(input).html(assessment_result.details[criteria]);
+			// 				});
+			// 			}
+			// 			result_table.find(`span[data-asset=${assessment_result.asset}].total-score-grade`).html(assessment_result.grade);
+			// 			let link_span = result_table.find(`span[data-asset=${assessment_result.asset}].total-result-link`);
+			// 			$(link_span).css("display", "block");
+			// 			$(link_span).find("a").attr("href", "/app/assessment-result/"+assessment_result.name);
+			// 		}
+			// 	});
+			// }
+		});
+	},
+
+	submit_result: function(frm) {
+		if (frm.doc.show_submit) {
+			frm.page.set_primary_action(__("Submit"), function() {
 				frappe.call({
-					method: "mfi_customization.mfi.doctype.machine_reading_tool.machine_reading_tool.mark_machine_reading",
+					method: "mfi_customization.mfi.doctype.machine_reading_tool.machine_reading_tool.create_machine_reading",
 					args: {
-						"project": frm.doc.project,
-						"machine_reading": machine_reading
+						"readings": asset_readings
 					},
 					callback: function(r) {
-						let reading_result = r.message;
-						console.log("@@@@@@@ reading_result",reading_result)
-						if (!frm.doc.show_submit) {
-							frm.doc.show_submit = true;
-							frm.events.submit_result;
+						if (r.message) {
+							frappe.msgprint(__("{0} Result submittted", [r.message]));
+						} else {
+							frappe.msgprint(__("No Result to submit"));
 						}
-						for (var criteria of Object.keys(reading_result.details)) {
-							result_table.find(`[data-criteria=${criteria}][data-readings=${reading_result
-								.readings}].readings-result-grade`).each(function(e1, input) {
-									$(input).html(reading_result.details[criteria]);
-							});
-						}
-						result_table.find(`span[data-readings=${reading_result.readings}].total-score-grade`).html(reading_result.grade);
-						let link_span = result_table.find(`span[data-readings=${reading_result.student}].total-result-link`);
-						$(link_span).css("display", "block");
-						$(link_span).find("a").attr("href", "/app/assessment-result/"+reading_result.name);
+						frm.events.project(frm);
 					}
 				});
-			}
-		});
+			});
+		}
+		else {
+			frm.page.clear_primary_action();
+		}
 	}
-
-
-
 });
+
