4166240cd3e42a41abda5d75d2616f25d0deaf55
tusharpatil3688 <tusharpatil3688>
2021-09-06 17:49:47 +0530

    fix:installation date validation on submit

--- mfi_customization/mfi/doctype/asset_installation_note/asset_installation_note.py
+++ mfi_customization/mfi/doctype/asset_installation_note/asset_installation_note.py
@@ -8,31 +8,29 @@ from frappe.utils import today
 class AssetInstallationNote(Document):
 	def on_submit(self):
 		submit_asset(self)
-		# create_stock_entry(self)
+		create_stock_entry(self)
 		# create_journal_entry(self)
+		validate_date(self)
 
 def create_stock_entry(doc):
-	warehouse={}
-	for d in  doc.get("model_serial_nos"):
-		warehouse.update({d.warehouse:(warehouse.get(d.warehouse) or "") +d.serial_no+","})
 	se=frappe.new_doc("Stock Entry")
 	se.company=frappe.db.get_value("Sales Order",{"name":doc.get('sales_order')},"company")
 	se.stock_entry_type="Material Issue"
 	se.project=doc.project
-	se.asset_delivery_note=doc.name
-	for d in warehouse:
-		for itm in doc.get("asset_models"):
+	se.asset_delivery_note=doc.asset_delivery_note
+	for ast in frappe.get_all("Asset",{"name":doc.asset},["item_code"]):
+		for itm in frappe.get_all("Item",{"name":ast.item_code},["stock_item"]):
 			se.append("items",{
-				"s_warehouse":d,
-				"item_code":itm.asset_model,
-				"qty":itm.qty,
-				"batch_no":doc.batch,
-				"serial_no":warehouse.get(d),
-				"basic_rate":frappe.db.get_value("Item",itm.asset_model,"valuation_rate") or 0,
-				"valuation_rate":frappe.db.get_value("Item",itm.asset_model,"valuation_rate") or 0
+				"s_warehouse":frappe.db.get_value("Serial No",doc.asset_serial_no,"warehouse"),
+				"item_code":itm.stock_item,
+				"qty":1,
+				"batch_no":frappe.db.get_value("Serial No",doc.asset_serial_no,"batch_no"),
+				"serial_no":doc.asset_serial_no,
+				"basic_rate":frappe.db.get_value("Item",itm.stock_item,"valuation_rate") or 0,
+				"valuation_rate":frappe.db.get_value("Item",itm.stock_item,"valuation_rate") or 0
 			})
-	se.save()
-	se.submit()
+			se.save()
+			se.submit()
 
 def create_journal_entry(doc):
 	total=0
@@ -62,4 +60,8 @@ def submit_asset(doc):
 		asset=frappe.get_doc("Asset",doc.asset)
 		if asset.docstatus==0:
 			asset.submit()
+
+def validate_date(doc):
+	if not doc.inst_date:
+		frappe.throw("Please Fill Installation Date")
 
