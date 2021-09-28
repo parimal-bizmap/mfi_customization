8d9d261e3274a5c5ca5790d38504cfb6f986a03e
tusharpatil3688 <tusharpatil3688>
2021-09-28 15:41:04 +0530

    fix:task readonly field bug

--- mfi_customization/public/js/task.js
+++ mfi_customization/public/js/task.js
@@ -304,12 +304,12 @@ validate:function(frm){
 frappe.ui.form.on("Asset Readings", "type", function(frm, cdt, cdn) {
 	var d = locals[cdt][cdn];
 	if (d.type=='Black & White'){
-        frm.set_df_property("reading", reqd, 1)
+        frm.set_df_property("reading", "reqd", 1)
 	$("div[data-idx='"+d.idx+"']").find("input[data-fieldname='reading_2']").css('pointer-events','none')
     $("div[data-idx='"+d.idx+"']").find("input[data-fieldname='reading']").css('pointer-events','all')
 	}
 	if (d.type=="Colour"){
-        frm.set_df_property("reading_2", reqd, 1)
+        frm.set_df_property("reading_2", "reqd", 1)
         $("div[data-idx='"+d.idx+"']").find("input[data-fieldname='reading_2']").css('pointer-events','all')
 		$("div[data-idx='"+d.idx+"']").find("input[data-fieldname='reading']").css('pointer-events','all')
 	}
